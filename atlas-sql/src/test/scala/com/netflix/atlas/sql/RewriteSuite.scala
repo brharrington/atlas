2ceb50d74434f1fa9d75b9786504f5c526dd155d
Brian Harrington <brharrington@gmail.com>
2016-03-19 15:12:53 -0700

    more sql tests

--- atlas-sql/src/test/scala/com/netflix/atlas/sql/RewriteSuite.scala
+++ atlas-sql/src/test/scala/com/netflix/atlas/sql/RewriteSuite.scala
@@ -61,6 +61,40 @@ class RewriteSuite extends FunSuite with BeforeAndAfterAll {
     }
   }
 
+  test("up vs read timeout") {
+    val results = context.sql(
+      """
+        |select name, dateint, hour, minute, sum(s1.value) as value
+        |from (
+        |  (
+        |    select tags['id'] as name, dateint, hour, minute, value
+        |    from atlas.atlas_p
+        |    lateral view posexplode(values) t as minute, value
+        |    where dateint > 20151201
+        |      and name = 'NumErrors'
+        |      and tags['class'] = 'NIWSErrorStats'
+        |      and (
+        |        tags['id'] = 'api-allrequests-ErrorCode-NIWSErrorCode-CONNECT_EXCEPTION'
+        |        or
+        |        tags['id'] = 'api-allrequests-ErrorCode-NIWSErrorCode-READ_TIMEOUT_EXCEPTION'
+        |      )
+        |  ) union all (
+        |    select name, dateint, hour, minute, value
+        |    from atlas.atlas_p
+        |    lateral view posexplode(values) t as minute, value
+        |    where dateint > 20151201
+        |      and name = 'DiscoveryStatus_api_UP'
+        |  )
+        |) s1
+        |where cast(s1.value as string) != 'NaN'
+        |group by name, dateint, hour, minute
+        |order by dateint, hour, minute;
+      """.stripMargin)
+    results.foreach { row =>
+      println(row)
+    }
+  }
+
   test("aggr2") {
     val results = context.sql("select name, i, v from atlas LATERAL view posexplode(values) t AS i, v")
     results.foreach { row =>
